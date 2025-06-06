3037d4a8f5b6d8d51d2de49961deee5aba2be116
ferryfitness <tyler.ferry03@gmail.com>
2025-06-06 14:40:12 -0700

    Delete src directory

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import FerryFitness from "./App";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<FerryFitness />);
